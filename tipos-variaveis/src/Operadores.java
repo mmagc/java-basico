7f20cbb6e79c7c5a897aa8acc5fa9cea706b076b
mmagc <matheusgnann@gmail.com>
2025-02-06 13:39:13 -0300

    Operador ternario

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,9 +2,18 @@ public class Operadores {
 
     public static void main(String[] args) {
 
-        boolean variavel = true;
+        int a, b;
+        a = 10;
+        b = 20;
+        String resultado = a==b ? "verdadeiro" : "falso";
 
-        System.out.println(!variavel);
+//        if(a==b)
+//            resultado = "verdadeiro";
+//        else
+//            resultado = "falso";
 
+
+
+        System.out.println(resultado);
     }
 }
