8f87a33e504cd25ccebddbadbdd24765a64e1df0
mmagc <matheusgnann@gmail.com>
2025-02-06 13:28:44 -0300

    Operadores aritmeticos adicionando numeros + for

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,15 +2,20 @@ public class Operadores {
 
     public static void main(String[] args) {
 
-        int numero = 5;
+        int numero = 1;
 
-        numero =  - numero;
+//        numero++;
 
-        System.out.println(numero);
+//        System.out.println(numero ++);
+//        System.out.println(++ numero);
+//
+//        System.out.println(numero --);
+//        System.out.println(-- numero);
 
-        numero = numero * -1;
-
-        System.out.println(numero);
+        for (int i = 1; i <= 10; i++) {
+            numero = i;
+            System.out.println(numero);
+        }
 
     }
 }
